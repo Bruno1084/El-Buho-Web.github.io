7ec66e769eeef5c9f17dea7e5d5f19766cc7aed7
Bruno Sosa <sosabruno3384@gmail.com>
2023-06-12 15:52:53 -0300

    json de carousel hecho

---
+++ Scripts/form-control.js
@@ -0,0 +1,5 @@
+const fName = document.getElementById('name');
+const fNumber = document.getElementById('number');
+const fEmail = document.getElementById('email');
+const fMessage = document.getElementById('message');
+
