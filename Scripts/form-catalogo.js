871a14b04ad946b3e66b4315ee83e340b69c6e6f
Bruno Sosa <sosabruno3384@gmail.com>
2023-06-14 20:01:41 -0300

    Creación de cartas de manera dinamica

--- Scripts/form-catalogo.js
+++
@@ -1,8 +0,0 @@
-const bt_Categorias= document.getElementsByName('fCategorias');
-
-const form = document.querySelector('#form-catalogo');
-form.addEventListener('change', (event) => {
-  const active = form.querySelector('.active');
-  if (active) active.classList.remove('active');
-  event.target.parentElement.classList.add('active');
-});
