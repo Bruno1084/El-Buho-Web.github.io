0a8612ebe623df51dcdddffe7eff606874fec8bb
Bruno Sosa <sosabruno3384@gmail.com>
2023-06-17 18:04:42 -0300

    Página de producto hecha

--- Scripts/card-creation.js
+++ Scripts/card-creation.js
@@ -7,6 +7,9 @@ const createCard=(productos)=> {
   contenedor_cartas.innerHTML= '';
     productos.forEach(producto => {
       const cardProducto = document.createElement('div');
+      var prod_item= `${producto.item}`
+      cardProducto.setAttribute("data-prod", prod_item);
+
       cardProducto.setAttribute("class", "card");
       var prodCategoria= `${producto.categoria}`;
       cardProducto.setAttribute("data-item", prodCategoria);
@@ -16,7 +19,7 @@ const createCard=(productos)=> {
         </div>
         <div class="card-body border-top">
           <div class="card-heading">
-            <a href="" class="text-decoration-none" id="card-tittle">${producto.nombre}</a>
+            <a href="" class="text-decoration-none" class="card-tittle">${producto.nombre}</a>
           </div>
           <div class="card-text text-muted">
             ${producto.descripción}
@@ -26,21 +29,26 @@ const createCard=(productos)=> {
             <p class="text-success" id="discount">${producto.descuento}</p>
           </div>
         </div>
-        <a class="bg-danger text-center text-decoration-none" href="#" onclick="compra_formulario()">
-          <p href="" class="card-button text-white">Comprar</p> 
+        <a class="prod-link bg-danger text-center text-decoration-none" onclick="sendProduct(${producto.item})" href="./product-page.html">
+          <p class="card-button text-white">Comprar</p> 
         </a>`;
       contenedor_cartas.appendChild(cardProducto);
     });
 }
 
 const getCardData=()=>{
-  fetch("https://bruno1084.github.io/TpMaquina-DW.github.io/Json/Productos.json")
+  fetch("../Json/Productos.json")
   .then(res => res.json())
   .then(res => {
     todos_los_productos= res;
     createCard(todos_los_productos);
   })
 }
+
+function sendProduct(item){
+  localStorage.setItem("producto", JSON.stringify(todos_los_productos[item]));
+}
+
 
 
 getCardData();
